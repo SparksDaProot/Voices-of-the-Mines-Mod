efd0c9ce085e6c330ef17daf6f6a4b1fc31d1cd0
Benj1kus <deathlimelp3@gmail.com>
2026-05-29 17:33:29 +0300

    garage door fix

--- src/main/java/net/votmdevs/voicesofthemines/block/GarageDoorBlock.java
+++ src/main/java/net/votmdevs/voicesofthemines/block/GarageDoorBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
@@ -43,6 +44,7 @@ public class GarageDoorBlock extends BaseEntityBlock {
         };
     }
 
+
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
         if (hand == InteractionHand.MAIN_HAND && player.getItemInHand(hand).isEmpty()) {
@@ -58,6 +60,15 @@ public class GarageDoorBlock extends BaseEntityBlock {
             return InteractionResult.sidedSuccess(level.isClientSide);
         }
         return InteractionResult.PASS;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        if (state.getValue(OPEN)) {
+            return Shapes.empty();
+        }
+
+        return this.getShape(state, level, pos, context);
     }
 
     @Override
